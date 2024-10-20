d2de96d1473370253f41d8f5a1931e656f8acbaf
Sergant073 <swtopic-2020@yandex.ru>
2024-10-20 12:39:01 +0400

    Повтор ДЗ!

---
+++ Module_1_5.py
@@ -0,0 +1,14 @@
+mutable_tuple = (['one', 2, 'three', 4, 5])
+immutable_var = ([1, 2, 3], ['lesson', 'lamp'])
+print(mutable_tuple, mutable_tuple[0])
+print(immutable_var)
+mutable_tuple[2] = 999
+mutable_tuple.append('Modified')
+print('Измененный кортеж:', mutable_tuple)
+
+# кортежи не изменяются по новым заданным значениям
+
+mutable_list = ["green", "red", "blue", "black"], ["bad", "sad", ]
+mutable_list[0][0] = "yellow"
+mutable_list[1][0] = "smile"
+print(mutable_list)
