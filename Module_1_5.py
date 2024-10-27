14ce15230e830457830ac33d66d774177c1f24b2
Sergant073 <swtopic-2020@yandex.ru>
2024-10-27 13:58:50 +0400

    Повтор ДЗ_2!

--- Module_1_5.py
+++ Module_1_5.py
@@ -1,9 +1,8 @@
-mutable_tuple = ([1, 2, 3, 4, 5])
+mutable_tuple = [1, 2, 3, 4, 5],
 immutable_var = (1, 'two', 3.0, [1, 2, 3, 4], True)
 print(mutable_tuple)
 print(immutable_var)
-mutable_tuple[2] = 999
-mutable_tuple.append('Modified')
+mutable_tuple[0][1] = 999
 print('Измененный кортеж:', mutable_tuple)
 
 # кортежи не изменяются по новым заданным значениям
@@ -13,24 +12,3 @@ mutable_list = ["green", "red", "blue", "black"], ["bad", "sad", ]
 mutable_list[0][0] = "yellow"
 mutable_list[1][0] = "smile"
 print(mutable_list)
-
-
-
-
-
-
-# mutable_tuple = (['one', 2, 'three', 4, 5])
-# immutable_var = ([1, 2, 3], ['lesson', 'lamp'])
-# print(mutable_tuple, mutable_tuple[0])
-# print(immutable_var)
-# mutable_tuple[2] = 999
-# mutable_tuple.append('Modified')
-# print('Измененный кортеж:', mutable_tuple)
-#
-# # кортежи не изменяются по новым заданным значениям
-#
-# mutable_list = ["green", "red", "blue", "black"], ["bad", "sad", ]
-# mutable_list[0][0] = "yellow"
-# mutable_list[1][0] = "smile"
-# print(mutable_list)
-
